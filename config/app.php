b4e2fedeab7a46a5ce18c8d9c1b799b7890ff5f9
nvladimir2019 <nosinov@gmail.com>
2020-08-16 13:21:05 +0300

    Lesson 2

--- config/app.php
+++ config/app.php
@@ -175,6 +175,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /** My Providers for this project */
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+
+
     ],
 
     /*
