008a05739bed7442b695a9af27ccaf88b603a134
nvladimir2019 <nosinov@gmail.com>
2020-08-22 22:32:34 +0300

    Create Bases controllers

---
+++ app/Models/Admin/User.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models\Admin;
+
+use App\Models\Role;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Model
+{
+    use SoftDeletes;
+    use Notifiable;
+
+    protected $fillable =
+        [
+            'name',
+            'login',
+            'email',
+            'password',
+        ];
+
+    protected $hidden =
+        [
+            'password',
+            'remember_token',
+        ];
+
+    protected $casts =
+        [
+            'email_verified_at' => 'datetime'
+        ];
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class,'user_roles');
+    }
+
+}
+
